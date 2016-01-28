ed2110f0fb3a1a11f5c012879020452b3b58b283
Daniele Santoro <psyacca@gmail.com>
2016-01-28 23:53:05 +0100

    add client project

---
+++ a3-client/src/introsde/assignment/soap/ObjectFactory.java
@@ -0,0 +1,457 @@
+
+package introsde.assignment.soap;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the introsde.assignment.soap package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _ReadPersonResponse_QNAME = new QName("http://soap.assignment.introsde/", "readPersonResponse");
+    private final static QName _SavePersonMeasurement_QNAME = new QName("http://soap.assignment.introsde/", "savePersonMeasurement");
+    private final static QName _DeletePersonResponse_QNAME = new QName("http://soap.assignment.introsde/", "deletePersonResponse");
+    private final static QName _ReadPersonHistory_QNAME = new QName("http://soap.assignment.introsde/", "readPersonHistory");
+    private final static QName _UpdatePersonResponse_QNAME = new QName("http://soap.assignment.introsde/", "updatePersonResponse");
+    private final static QName _ReadPersonHistoryResponse_QNAME = new QName("http://soap.assignment.introsde/", "readPersonHistoryResponse");
+    private final static QName _ReadPersonListResponse_QNAME = new QName("http://soap.assignment.introsde/", "readPersonListResponse");
+    private final static QName _SavePersonMeasurementResponse_QNAME = new QName("http://soap.assignment.introsde/", "savePersonMeasurementResponse");
+    private final static QName _Person_QNAME = new QName("http://soap.assignment.introsde/", "person");
+    private final static QName _ReadPerson_QNAME = new QName("http://soap.assignment.introsde/", "readPerson");
+    private final static QName _ReadPersonList_QNAME = new QName("http://soap.assignment.introsde/", "readPersonList");
+    private final static QName _Measure_QNAME = new QName("http://soap.assignment.introsde/", "Measure");
+    private final static QName _MeasureDefaultRange_QNAME = new QName("http://soap.assignment.introsde/", "measureDefaultRange");
+    private final static QName _UpdatePerson_QNAME = new QName("http://soap.assignment.introsde/", "updatePerson");
+    private final static QName _ReadPersonMeasurementResponse_QNAME = new QName("http://soap.assignment.introsde/", "readPersonMeasurementResponse");
+    private final static QName _HealthMeasureHistory_QNAME = new QName("http://soap.assignment.introsde/", "healthMeasureHistory");
+    private final static QName _CreatePersonResponse_QNAME = new QName("http://soap.assignment.introsde/", "createPersonResponse");
+    private final static QName _DeletePerson_QNAME = new QName("http://soap.assignment.introsde/", "deletePerson");
+    private final static QName _ReadMeasureTypesResponse_QNAME = new QName("http://soap.assignment.introsde/", "readMeasureTypesResponse");
+    private final static QName _ReadMeasureTypes_QNAME = new QName("http://soap.assignment.introsde/", "readMeasureTypes");
+    private final static QName _CreatePerson_QNAME = new QName("http://soap.assignment.introsde/", "createPerson");
+    private final static QName _MeasureDefinition_QNAME = new QName("http://soap.assignment.introsde/", "measureDefinition");
+    private final static QName _ReadPersonMeasurement_QNAME = new QName("http://soap.assignment.introsde/", "readPersonMeasurement");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: introsde.assignment.soap
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Person }
+     * 
+     */
+    public Person createPerson() {
+        return new Person();
+    }
+
+    /**
+     * Create an instance of {@link DeletePerson }
+     * 
+     */
+    public DeletePerson createDeletePerson() {
+        return new DeletePerson();
+    }
+
+    /**
+     * Create an instance of {@link CreatePersonResponse }
+     * 
+     */
+    public CreatePersonResponse createCreatePersonResponse() {
+        return new CreatePersonResponse();
+    }
+
+    /**
+     * Create an instance of {@link HealthMeasureHistory }
+     * 
+     */
+    public HealthMeasureHistory createHealthMeasureHistory() {
+        return new HealthMeasureHistory();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonMeasurementResponse }
+     * 
+     */
+    public ReadPersonMeasurementResponse createReadPersonMeasurementResponse() {
+        return new ReadPersonMeasurementResponse();
+    }
+
+    /**
+     * Create an instance of {@link ReadMeasureTypesResponse }
+     * 
+     */
+    public ReadMeasureTypesResponse createReadMeasureTypesResponse() {
+        return new ReadMeasureTypesResponse();
+    }
+
+    /**
+     * Create an instance of {@link CreatePerson }
+     * 
+     */
+    public CreatePerson createCreatePerson() {
+        return new CreatePerson();
+    }
+
+    /**
+     * Create an instance of {@link MeasureDefinition }
+     * 
+     */
+    public MeasureDefinition createMeasureDefinition() {
+        return new MeasureDefinition();
+    }
+
+    /**
+     * Create an instance of {@link ReadMeasureTypes }
+     * 
+     */
+    public ReadMeasureTypes createReadMeasureTypes() {
+        return new ReadMeasureTypes();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonMeasurement }
+     * 
+     */
+    public ReadPersonMeasurement createReadPersonMeasurement() {
+        return new ReadPersonMeasurement();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonHistoryResponse }
+     * 
+     */
+    public ReadPersonHistoryResponse createReadPersonHistoryResponse() {
+        return new ReadPersonHistoryResponse();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonListResponse }
+     * 
+     */
+    public ReadPersonListResponse createReadPersonListResponse() {
+        return new ReadPersonListResponse();
+    }
+
+    /**
+     * Create an instance of {@link UpdatePersonResponse }
+     * 
+     */
+    public UpdatePersonResponse createUpdatePersonResponse() {
+        return new UpdatePersonResponse();
+    }
+
+    /**
+     * Create an instance of {@link DeletePersonResponse }
+     * 
+     */
+    public DeletePersonResponse createDeletePersonResponse() {
+        return new DeletePersonResponse();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonHistory }
+     * 
+     */
+    public ReadPersonHistory createReadPersonHistory() {
+        return new ReadPersonHistory();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonResponse }
+     * 
+     */
+    public ReadPersonResponse createReadPersonResponse() {
+        return new ReadPersonResponse();
+    }
+
+    /**
+     * Create an instance of {@link SavePersonMeasurement }
+     * 
+     */
+    public SavePersonMeasurement createSavePersonMeasurement() {
+        return new SavePersonMeasurement();
+    }
+
+    /**
+     * Create an instance of {@link ReadPersonList }
+     * 
+     */
+    public ReadPersonList createReadPersonList() {
+        return new ReadPersonList();
+    }
+
+    /**
+     * Create an instance of {@link ReadPerson }
+     * 
+     */
+    public ReadPerson createReadPerson() {
+        return new ReadPerson();
+    }
+
+    /**
+     * Create an instance of {@link SavePersonMeasurementResponse }
+     * 
+     */
+    public SavePersonMeasurementResponse createSavePersonMeasurementResponse() {
+        return new SavePersonMeasurementResponse();
+    }
+
+    /**
+     * Create an instance of {@link Measure }
+     * 
+     */
+    public Measure createMeasure() {
+        return new Measure();
+    }
+
+    /**
+     * Create an instance of {@link UpdatePerson }
+     * 
+     */
+    public UpdatePerson createUpdatePerson() {
+        return new UpdatePerson();
+    }
+
+    /**
+     * Create an instance of {@link MeasureDefaultRange }
+     * 
+     */
+    public MeasureDefaultRange createMeasureDefaultRange() {
+        return new MeasureDefaultRange();
+    }
+
+    /**
+     * Create an instance of {@link Person.CurrenHealth }
+     * 
+     */
+    public Person.CurrenHealth createPersonCurrenHealth() {
+        return new Person.CurrenHealth();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonResponse")
+    public JAXBElement<ReadPersonResponse> createReadPersonResponse(ReadPersonResponse value) {
+        return new JAXBElement<ReadPersonResponse>(_ReadPersonResponse_QNAME, ReadPersonResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SavePersonMeasurement }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "savePersonMeasurement")
+    public JAXBElement<SavePersonMeasurement> createSavePersonMeasurement(SavePersonMeasurement value) {
+        return new JAXBElement<SavePersonMeasurement>(_SavePersonMeasurement_QNAME, SavePersonMeasurement.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeletePersonResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "deletePersonResponse")
+    public JAXBElement<DeletePersonResponse> createDeletePersonResponse(DeletePersonResponse value) {
+        return new JAXBElement<DeletePersonResponse>(_DeletePersonResponse_QNAME, DeletePersonResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonHistory }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonHistory")
+    public JAXBElement<ReadPersonHistory> createReadPersonHistory(ReadPersonHistory value) {
+        return new JAXBElement<ReadPersonHistory>(_ReadPersonHistory_QNAME, ReadPersonHistory.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link UpdatePersonResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "updatePersonResponse")
+    public JAXBElement<UpdatePersonResponse> createUpdatePersonResponse(UpdatePersonResponse value) {
+        return new JAXBElement<UpdatePersonResponse>(_UpdatePersonResponse_QNAME, UpdatePersonResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonHistoryResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonHistoryResponse")
+    public JAXBElement<ReadPersonHistoryResponse> createReadPersonHistoryResponse(ReadPersonHistoryResponse value) {
+        return new JAXBElement<ReadPersonHistoryResponse>(_ReadPersonHistoryResponse_QNAME, ReadPersonHistoryResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonListResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonListResponse")
+    public JAXBElement<ReadPersonListResponse> createReadPersonListResponse(ReadPersonListResponse value) {
+        return new JAXBElement<ReadPersonListResponse>(_ReadPersonListResponse_QNAME, ReadPersonListResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SavePersonMeasurementResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "savePersonMeasurementResponse")
+    public JAXBElement<SavePersonMeasurementResponse> createSavePersonMeasurementResponse(SavePersonMeasurementResponse value) {
+        return new JAXBElement<SavePersonMeasurementResponse>(_SavePersonMeasurementResponse_QNAME, SavePersonMeasurementResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Person }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "person")
+    public JAXBElement<Person> createPerson(Person value) {
+        return new JAXBElement<Person>(_Person_QNAME, Person.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPerson }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPerson")
+    public JAXBElement<ReadPerson> createReadPerson(ReadPerson value) {
+        return new JAXBElement<ReadPerson>(_ReadPerson_QNAME, ReadPerson.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonList }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonList")
+    public JAXBElement<ReadPersonList> createReadPersonList(ReadPersonList value) {
+        return new JAXBElement<ReadPersonList>(_ReadPersonList_QNAME, ReadPersonList.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Measure }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "Measure")
+    public JAXBElement<Measure> createMeasure(Measure value) {
+        return new JAXBElement<Measure>(_Measure_QNAME, Measure.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MeasureDefaultRange }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "measureDefaultRange")
+    public JAXBElement<MeasureDefaultRange> createMeasureDefaultRange(MeasureDefaultRange value) {
+        return new JAXBElement<MeasureDefaultRange>(_MeasureDefaultRange_QNAME, MeasureDefaultRange.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link UpdatePerson }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "updatePerson")
+    public JAXBElement<UpdatePerson> createUpdatePerson(UpdatePerson value) {
+        return new JAXBElement<UpdatePerson>(_UpdatePerson_QNAME, UpdatePerson.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonMeasurementResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonMeasurementResponse")
+    public JAXBElement<ReadPersonMeasurementResponse> createReadPersonMeasurementResponse(ReadPersonMeasurementResponse value) {
+        return new JAXBElement<ReadPersonMeasurementResponse>(_ReadPersonMeasurementResponse_QNAME, ReadPersonMeasurementResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link HealthMeasureHistory }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "healthMeasureHistory")
+    public JAXBElement<HealthMeasureHistory> createHealthMeasureHistory(HealthMeasureHistory value) {
+        return new JAXBElement<HealthMeasureHistory>(_HealthMeasureHistory_QNAME, HealthMeasureHistory.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreatePersonResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "createPersonResponse")
+    public JAXBElement<CreatePersonResponse> createCreatePersonResponse(CreatePersonResponse value) {
+        return new JAXBElement<CreatePersonResponse>(_CreatePersonResponse_QNAME, CreatePersonResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeletePerson }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "deletePerson")
+    public JAXBElement<DeletePerson> createDeletePerson(DeletePerson value) {
+        return new JAXBElement<DeletePerson>(_DeletePerson_QNAME, DeletePerson.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadMeasureTypesResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readMeasureTypesResponse")
+    public JAXBElement<ReadMeasureTypesResponse> createReadMeasureTypesResponse(ReadMeasureTypesResponse value) {
+        return new JAXBElement<ReadMeasureTypesResponse>(_ReadMeasureTypesResponse_QNAME, ReadMeasureTypesResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadMeasureTypes }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readMeasureTypes")
+    public JAXBElement<ReadMeasureTypes> createReadMeasureTypes(ReadMeasureTypes value) {
+        return new JAXBElement<ReadMeasureTypes>(_ReadMeasureTypes_QNAME, ReadMeasureTypes.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link CreatePerson }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "createPerson")
+    public JAXBElement<CreatePerson> createCreatePerson(CreatePerson value) {
+        return new JAXBElement<CreatePerson>(_CreatePerson_QNAME, CreatePerson.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link MeasureDefinition }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "measureDefinition")
+    public JAXBElement<MeasureDefinition> createMeasureDefinition(MeasureDefinition value) {
+        return new JAXBElement<MeasureDefinition>(_MeasureDefinition_QNAME, MeasureDefinition.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link ReadPersonMeasurement }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://soap.assignment.introsde/", name = "readPersonMeasurement")
+    public JAXBElement<ReadPersonMeasurement> createReadPersonMeasurement(ReadPersonMeasurement value) {
+        return new JAXBElement<ReadPersonMeasurement>(_ReadPersonMeasurement_QNAME, ReadPersonMeasurement.class, null, value);
+    }
+
+}
---
+++ a3-client/src/introsde/assignment/soap/Person.java
@@ -0,0 +1,288 @@
+
+package introsde.assignment.soap;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for person complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="person">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="birthdate" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
+ *         &lt;element name="email" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="idPerson" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="lastname" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="currenHealth" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;element name="measure" type="{http://soap.assignment.introsde/}measure" maxOccurs="unbounded" minOccurs="0"/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="username" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "person", propOrder = {
+    "birthdate",
+    "email",
+    "idPerson",
+    "lastname",
+    "currenHealth",
+    "name",
+    "username"
+})
+public class Person {
+
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar birthdate;
+    protected String email;
+    protected int idPerson;
+    protected String lastname;
+    protected Person.CurrenHealth currenHealth;
+    protected String name;
+    protected String username;
+
+    /**
+     * Gets the value of the birthdate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getBirthdate() {
+        return birthdate;
+    }
+
+    /**
+     * Sets the value of the birthdate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setBirthdate(XMLGregorianCalendar value) {
+        this.birthdate = value;
+    }
+
+    /**
+     * Gets the value of the email property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Sets the value of the email property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Gets the value of the idPerson property.
+     * 
+     */
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    /**
+     * Sets the value of the idPerson property.
+     * 
+     */
+    public void setIdPerson(int value) {
+        this.idPerson = value;
+    }
+
+    /**
+     * Gets the value of the lastname property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLastname() {
+        return lastname;
+    }
+
+    /**
+     * Sets the value of the lastname property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLastname(String value) {
+        this.lastname = value;
+    }
+
+    /**
+     * Gets the value of the currenHealth property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Person.CurrenHealth }
+     *     
+     */
+    public Person.CurrenHealth getCurrenHealth() {
+        return currenHealth;
+    }
+
+    /**
+     * Sets the value of the currenHealth property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Person.CurrenHealth }
+     *     
+     */
+    public void setCurrenHealth(Person.CurrenHealth value) {
+        this.currenHealth = value;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the username property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets the value of the username property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUsername(String value) {
+        this.username = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;element name="measure" type="{http://soap.assignment.introsde/}measure" maxOccurs="unbounded" minOccurs="0"/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "measure"
+    })
+    public static class CurrenHealth {
+
+        protected List<Measure> measure;
+
+        /**
+         * Gets the value of the measure property.
+         * 
+         * <p>
+         * This accessor method returns a reference to the live list,
+         * not a snapshot. Therefore any modification you make to the
+         * returned list will be present inside the JAXB object.
+         * This is why there is not a <CODE>set</CODE> method for the measure property.
+         * 
+         * <p>
+         * For example, to add a new item, do as follows:
+         * <pre>
+         *    getMeasure().add(newItem);
+         * </pre>
+         * 
+         * 
+         * <p>
+         * Objects of the following type(s) are allowed in the list
+         * {@link Measure }
+         * 
+         * 
+         */
+        public List<Measure> getMeasure() {
+            if (measure == null) {
+                measure = new ArrayList<Measure>();
+            }
+            return this.measure;
+        }
+
+    }
+
+}
